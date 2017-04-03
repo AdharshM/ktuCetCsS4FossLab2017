837a67071b8dc24af79eb12899544cc51b42bb54
Jilvin <jilvinjacob@gmail.com>
2017-04-03 11:10:38 +0530

    added third experiment

---
+++ experiment_3/script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#Currently logged user and his logname
+echo $USER
+
+#Your current shell
+echo $0
+
+#change to curent user's home directory
+cd ~
+
+#Your operating system type
+uname -a
+
+#Your current path setting
+echo $PATH
+
+#Your current working directory
+pwd
+
+#Show Currently logged number of users
+users
+
+
+
